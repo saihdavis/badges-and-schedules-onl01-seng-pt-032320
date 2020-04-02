bfaaae390f6483bf093437de27c456c2475a9a50
Saida Davis <saihdavis@gmail.com>
2020-04-02 20:17:04 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,8 @@
-# Write your code here.
+def badge_maker(name)
+    ("Hello, my name is #{name}.")
+end
+
+def batch_badge_creator(attendees)
+  attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  attendees.each{|attendee| puts "Hello, my name is #{attendee}."}
+end
