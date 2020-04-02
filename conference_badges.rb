deba8c55038135968df7ff06a403071548e0fa46
Saida Davis <saihdavis@gmail.com>
2020-04-02 20:17:36 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,5 +4,5 @@ end
 
 def batch_badge_creator(attendees)
   attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-  attendees.each{|attendee| puts "Hello, my name is #{attendee}."}
+  attendees.collect{|attendee| puts "Hello, my name is #{attendee}."}
 end
